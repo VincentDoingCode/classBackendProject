0263160abe79faee47c7d326ba0f2fdb09e14a57
VincentDoingCode <113548490+VincentDoingCode@users.noreply.github.com>
2023-01-19 21:41:49 -0500

    Add files via upload

--- backend_http/ex.js
+++ backend_http/ex.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+
+//enable a feature needed for POST request
+app.use(express.json());
+
+//GET requests
 app.get('/',(req,res)=>{
     res.send('Hello there');
 });
@@ -10,6 +15,7 @@ const courses = [
     {id:3,name:'Cybersecurity'}
 ];
 
+//GET requests
 app.get('/api/courses',(req,res)=>{
     res.send(courses);
 });
@@ -22,6 +28,49 @@ app.get('/api/courses:id',(req,res)=>{
     }
         res.send(course);
 });
+
+//POST requests
+app.post('/api/courses',(req,res)=>{
+    if(req.body.name.length > 3){
+        const course={
+            id:courses.length +1,
+            name: req.body.name
+        }
+        courses.push(course);
+        res.send(course);
+    }
+    else{
+        res.send("name is required and it should be a minimum of 3 characters");
+    }
+});
+
+//PUT requests
+app.put('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404).send('The course with the given ID was not found');
+        return
+    }
+    const pos = courses.findIndex(c => c.id === parseInt(req.params.id));
+    const put = {
+        id:req.params.id,
+        name:req.body.name
+    };
+    courses[pos] = put;
+    res.send(put);
+})
+
+//DELETE requests
+app.delete('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c =>c.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404).send("The course with the given ID was not found");
+        return
+    }
+    const pos = courses.findIndex(c => c.id === parseInt(req.params.id));
+    courses.splice(pos,1);
+    res.send(courses[pos]);
+})
 
 app.listen(3000, ()=>{
     console.log('Listening on port 3000')
---
+++ backend_http/reflection.js
@@ -0,0 +1,7 @@
+//Different program communicate base on how to user interact with the frontend.
+//Of course the GET request wouuld be requested first to get all the information available for the users.
+//Then according to the user's intent, the other requests are really just in the user's hand.
+//Get requests just get the information like cracking an egg to get the yolk inside. 
+//Post requests adds information to the backend like adding a piece of puzzle to an unfinished puzzle board.
+//Put requests replace an existing information in the backend like replacing your iphone simply because you want a new one. 
+//Delete request are similar to just trashing a file on the computer. 
